a5d9c9b700c5da368c66e6966a1fa57de428eee3
Robert Hughes <nz.r.hughes@gmail.com>
2021-07-14 16:48:13 +1200

    Added some Basic Structure

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 import logging
+from utils import hex2int as h2i
+
+def init():
+    logging.basicConfig(level=logging.DEBUG)
---
+++ memory.py
@@ -0,0 +1,19 @@
+import logging
+from utils import hex2int as h2i
+
+class Memory:
+    memory = bytearray(65536)
+
+    def __init__(self):
+        for i in range(len(self.memory)):
+            self.memory[i] = 0
+
+    def load(self, data, offset):
+        logging.debug("Loading data to memory starting at {offset}")
+        for i in range(len(data)):
+            self.memory[int(i+offset)] = data[i]
+    
+    def printmem(self, mode="hex"):
+        if mode == hex:
+            for i in range(len(self.memory)):
+                print("{0:X}:\t{1:02X}\t{2:02X}".format(i, self.memory[i], self.memory[i+1]))
